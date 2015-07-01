52312508156f515aea4a2a86f5c990ad32fda765
May Pongpitpitak <mayppong@gmail.com>
2015-07-01 22:05:56 +0000

    Add portal door to the supervision trees.

--- lib/portal.ex
+++ lib/portal.ex
@@ -11,12 +11,20 @@ defmodule Portal do
     children = [
       # Define workers and child supervisors to be supervised
       # worker(Portal.Worker, [arg1, arg2, arg3])
+      worker(Portal.Door, [])
     ]
 
     # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
     # for other strategies and supported options
-    opts = [strategy: :one_for_one, name: Portal.Supervisor]
+    opts = [strategy: :simple_one_for_one, name: Portal.Supervisor]
     Supervisor.start_link(children, opts)
+  end
+
+  @doc """
+  Shoot a new door with a given `color`.
+  """
+  def shoot(color) do
+    Supervisor.start_child(Portal.Supervisor, [color])
   end
 
   @doc """
