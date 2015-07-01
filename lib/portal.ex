7bcb90ff7a39b3c232df83bc4b75b295cf1b4a64
May Pongpitpitak <mayppong@gmail.com>
2015-07-01 02:50:21 +0000

    Extend Inspect for better console print out

--- lib/portal.ex
+++ lib/portal.ex
@@ -18,7 +18,6 @@ defmodule Portal do
     opts = [strategy: :one_for_one, name: Portal.Supervisor]
     Supervisor.start_link(children, opts)
   end
-end
 
   @doc """
   Starts transfering `data` from `left` to `right`.
@@ -46,5 +45,24 @@ end
 
     # Let's return the portal itself
     portal
+  end
+end
+
+defimpl Inspect, for: Portal do
+  def inspect(%Portal{left: left, right: right}, _) do
+    left_door = inspect(left)
+    right_door = inspect(right)
+
+    left_data = inspect(Enum.reverse(Portal.Door.get(left)))
+    right_data = inspect(Portal.Door.get(right))
+
+    max = max(String.length(left_door), String.length(left_data))
+
+    """
+    #Portal<
+      #{String.rjust(left_door, max)} <=> #{right_door}
+      #{String.rjust(left_data, max)} <=> #{right_data}
+    >
+    """
   end
 end
